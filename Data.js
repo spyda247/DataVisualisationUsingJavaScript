837784f874e44bc747124636b2e776cb8fa6591a
Babajide Ibiayo <babajideibiayo@yahoo.com>
2023-06-06 06:13:25 +0100

    Initial commit

---
+++ Data.js
@@ -0,0 +1,214 @@
+export function getData() {
+  return [
+    {
+      x: "Monday",
+      y: "Midnight-3:59 a.m.",
+      heat: 705
+    },
+    {
+      x: "Monday",
+      y: "4:00-7:59 a.m.",
+      heat: 713
+    },
+    {
+      x: "Monday",
+      y: "8:00-11:59 a.m.",
+      heat: 657
+    },
+    {
+      x: "Monday",
+      y: "Noon-3:59 p.m.",
+      heat: 957
+    },
+    {
+      x: "Monday",
+      y: "4:00-7:59 p.m.",
+      heat: 1137
+    },
+    {
+      x: "Monday",
+      y: "8:00-11:59 p.m.",
+      heat: 956
+    },
+    {
+      x: "Tuesday",
+      y: "Midnight-3:59 a.m.",
+      heat: 482
+    },
+    {
+      x: "Tuesday",
+      y: "4:00-7:59 a.m.",
+      heat: 641
+    },
+    {
+      x: "Tuesday",
+      y: "8:00-11:59 a.m.",
+      heat: 631
+    },
+    {
+      x: "Tuesday",
+      y: "Noon-3:59 p.m.",
+      heat: 905
+    },
+    {
+      x: "Tuesday",
+      y: "4:00-7:59 p.m.",
+      heat: 1137
+    },
+    {
+      x: "Tuesday",
+      y: "8:00-11:59 p.m.",
+      heat: 986
+    },
+    {
+      x: "Wednesday",
+      y: "Midnight-3:59 a.m.",
+      heat: 465
+    },
+    {
+      x: "Wednesday",
+      y: "4:00-7:59 a.m.",
+      heat: 616
+    },
+    {
+      x: "Wednesday",
+      y: "8:00-11:59 a.m.",
+      heat: 627
+    },
+    {
+      x: "Wednesday",
+      y: "Noon-3:59 p.m.",
+      heat: 914
+    },
+    {
+      x: "Wednesday",
+      y: "4:00-7:59 p.m.",
+      heat: 1159
+    },
+    {
+      x: "Wednesday",
+      y: "8:00-11:59 p.m.",
+      heat: 1066
+    },
+    {
+      x: "Thursday",
+      y: "Midnight-3:59 a.m.",
+      heat: 584
+    },
+    {
+      x: "Thursday",
+      y: "4:00-7:59 a.m.",
+      heat: 718
+    },
+    {
+      x: "Thursday",
+      y: "8:00-11:59 a.m.",
+      heat: 660
+    },
+    {
+      x: "Thursday",
+      y: "Noon-3:59 p.m.",
+      heat: 966
+    },
+    {
+      x: "Thursday",
+      y: "4:00-7:59 p.m.",
+      heat: 1161
+    },
+    {
+      x: "Thursday",
+      y: "8:00-11:59 p.m.",
+      heat: 1186
+    },
+    {
+      x: "Friday",
+      y: "Midnight-3:59 a.m.",
+      heat: 715
+    },
+    {
+      x: "Friday",
+      y: "4:00-7:59 a.m.",
+      heat: 747
+    },
+    {
+      x: "Friday",
+      y: "8:00-11:59 a.m.",
+      heat: 738
+    },
+    {
+      x: "Friday",
+      y: "Noon-3:59 p.m.",
+      heat: 1056
+    },
+    {
+      x: "Friday",
+      y: "4:00-7:59 p.m.",
+      heat: 1426
+    },
+    {
+      x: "Friday",
+      y: "8:00-11:59 p.m.",
+      heat: 1631
+    },
+    {
+      x: "Saturday",
+      y: "Midnight-3:59 a.m.",
+      heat: 1383
+    },
+    {
+      x: "Saturday",
+      y: "4:00-7:59 a.m.",
+      heat: 641
+    },
+    {
+      x: "Saturday",
+      y: "8:00-11:59 a.m.",
+      heat: 635
+    },
+    {
+      x: "Saturday",
+      y: "Noon-3:59 p.m.",
+      heat: 1034
+    },
+    {
+      x: "Saturday",
+      y: "4:00-7:59 p.m.",
+      heat: 1400
+    },
+    {
+      x: "Saturday",
+      y: "8:00-11:59 p.m.",
+      heat: 1593
+    },
+    {
+      x: "Sunday",
+      y: "Midnight-3:59 a.m.",
+      heat: 1486
+    },
+    {
+      x: "Sunday",
+      y: "4:00-7:59 a.m.",
+      heat: 695
+    },
+    {
+      x: "Sunday",
+      y: "8:00-11:59 a.m.",
+      heat: 564
+    },
+    {
+      x: "Sunday",
+      y: "Noon-3:59 p.m.",
+      heat: 932
+    },
+    {
+      x: "Sunday",
+      y: "4:00-7:59 p.m.",
+      heat: 1292
+    },
+    {
+      x: "Sunday",
+      y: "8:00-11:59 p.m.",
+      heat: 1211
+    }
+  ];
+}
